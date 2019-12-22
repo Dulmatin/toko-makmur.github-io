899a68c3de0833b035f8766cce0cacd29bea9f67
unknown <babangryan23@gmail.com>
2019-12-22 07:29:48 -0800

    Penambahan migration dan seed Product

---
+++ database/migrations/2019_12_22_145343_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('unit_id');
+            $table->integer('categori_id');
+            $table->integer('stok');
+            $table->integer('purchase_price');
+            $table->integer('sell_price');
+            $table->string('image');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       	Product::insert(
+       		[
+       			[
+       				'name' => 'ember' , 
+       				'unit_id' => '1' ,
+       				'categori_id' => '1' ,
+       				'stok' => '10' ,
+       				'purchase_price' => '1000' ,
+       				'sell_price' => '1500' ,
+       				'image' => 'exsample'
+       			],
+       			[
+       				'name' => 'panci' , 
+       				'unit_id' => '1' ,
+       				'categori_id' => '1' ,
+       				'stok' => '10' ,
+       				'purchase_price' => '2000' ,
+       				'sell_price' => '3000' ,
+       				'image' => 'exsample'
+       			],
+       			[
+       				'name' => 'sodet' , 
+       				'unit_id' => '1' ,
+       				'categori_id' => '1' ,
+       				'stok' => '10' ,
+       				'purchase_price' => '1500' ,
+       				'sell_price' => '2000' ,
+       				'image' => 'exsample'
+       			]
+       		]
+       	);
+    }
+}
